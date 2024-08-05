cb19c78023470a31bc489c71d3d17e0f0922f2eb
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-08-05 15:30:01 +0530

    .

--- exponance/src/main.jsx
+++ exponance/src/main.jsx
@@ -5,12 +5,11 @@ import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+ 
   <React.StrictMode>
     <ChakraProvider>
     < App/>
     <ChakraProvider/>
-  <React.StrictMode/>,
+  <React.StrictMode/>
 
-  
-)
+  )
